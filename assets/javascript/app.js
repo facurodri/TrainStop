17412a5f9867eb5ffe82dda5aebb813da2a66ae7
Facundo Rodriguez <facurodri91@gmail.com>
2019-03-10 23:57:25 -0400

    working on time/math

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,32 +9,70 @@
   firebase.initializeApp(config);
   //varible to hold data from firebase
   var database = firebase.database();
+  
   //submit button for train form 
   $("#addTrainBtn").on("click", function (event) {
-    event.preventDefault();
-    //creates varibles to hold inputs 
-    var trainName = $("#traiName").val().trim();
-    var destination = $("#destination").val().trim();
-    var firstTrain = $("#firstTrain").val().trim();
-    var frequency = $("#frequency").val().trim();
-    //Updates data to firebase storage
-    console.log(trainName);
-    console.log(destination);
-    console.log(firstTrain);
-    console.log(frequency);
-    database.ref().push({
-        name: trainName,
-        destination: destination,
-        firstTrain: firstTrain,
-        frequency: frequency
+      event.preventDefault();
+      //creates varibles to hold inputs 
+      var trainName = $("#trainName").val().trim();
+      var destination = $("#destination").val().trim();
+      var firstTrain = $("#firstTrain").val().trim();
+      var frequency = $("#frequency").val().trim();
+      // var timeLeft = $("timeLeft").val("?");
+      console.log(trainName);
+      console.log(destination);
+      console.log(firstTrain);
+      console.log(frequency);
+      //Updates data to firebase storage
+      database.ref().push({
+          name: trainName,
+          destination: destination,
+          firstTrain: firstTrain,
+          frequency: frequency
+        });
+        
+        alert("Train successfully added");
+        
+        // Clears all of the text-boxes
+        $("#trainName").val("");
+        $("#destination").val("");
+        $("#firstTrain").val("");
+        $("#frequency").val("");
+                
+    
     });
+    // var timeLeft = $("timeLeft").text("?");
+    
+    database.ref().on("child_added", function(childSnapspot){
+        console.log(childSnapspot.key);
+        console.log(childSnapspot.val());
+        
+        var trainName = childSnapspot.val().name;
+        var destination = childSnapspot.val().destination;
+        var firstTrain = childSnapspot.val().firstTrain;
+        var frequency = childSnapspot.val().frequency;
+       
+        var timeOfArrival;
+        var timeOfMinutesPasssed;
 
-    alert("Train successfully added");
-
-    // Clears all of the text-boxes
-    $("#trainName").val("");
-    $("#destination").val("");
-    $("#firstTrain").val("");
-    $("#frequency").val("");
-});
-
+        var trainTime= moment(firstTrain,"hh:mm").subtract(1,"day");
+        
+        console.log(trainName);
+        console.log(destination);
+        console.log(firstTrain);
+        console.log(frequency);
+        
+        // create new row
+        var newRow = $("<tr>").append(
+            $("<td>").text(trainName),
+            $("<td>").text(destination),
+            $("<td>").text(firstTrain),
+            $("<td>").text(frequency),
+            $("<td>").text("?")
+            
+            
+            );
+            
+            $("#train-table > tbody").append(newRow); 
+        });
+        
